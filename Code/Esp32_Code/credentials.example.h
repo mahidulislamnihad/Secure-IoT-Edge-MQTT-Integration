b4a9122555dde087aace2932ed7e66f127845f55
Mahidul Islam Nihad <mahidulislamnihad28@gmail.com>
2025-11-12 12:32:23 +0600

    Create credentials.example.h
    Change it as per your setup

---
+++ Code/Esp32_Code/credentials.example.h
@@ -0,0 +1,14 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Wi-Fi
+const char* WIFI_SSID = "WIFI_SSID";
+const char* WIFI_PASS = "WIFI_PASSWORD";
+
+// MQTT Config
+const char* MQTT_SERVER = "Broker IP";
+const int   MQTT_PORT   = 8883; // Since TLS Encryption is enable 
+const char* MQTT_USER   = "MQTT_USERNAME";
+const char* MQTT_PASS   = "MQTT_PASSWORD";
+
+#endif
