595a65a8ea8e1a5dc05dc62e0b011341fb99e5da
Mahidul Islam Nihad <mahidulislamnihad28@gmail.com>
2025-11-12 12:35:06 +0600

    Create ca_cert.h

---
+++ Code/Esp32_Code/ca_cert.h
@@ -0,0 +1,8 @@
+const char ca_cert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+[Paste your generated CA certificate content here]
+-----END CERTIFICATE-----
+)EOF";
+
+
+****You just can copy your file and name it ca_cert.h  insted of copy pasting content here*****
